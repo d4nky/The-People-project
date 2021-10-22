3857b3082f2349547d30f8ebb4292dd641da106a
d4nky <vertix08@gmail.com>
2021-10-22 12:32:13 +0100

    First commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-onclick(MouseEvent):document.getElementsByClassName('menu-button');
+let menuBtn = document.querySelector(".menu-button");
+let menu = document.querySelector(".dropdown-menu");
+let menuOpen = false;
+
+function menuToggle(){
+    if(menuOpen == false){
+        menu.style.right = '-500px';
+        menuOpen = true;
+    }
+    else if(menuOpen == true){
+        menu.style.right = "0px";
+        menuOpen = false;
+    }
+}
+
+
+menuBtn.onclick = menuToggle;
+
+
