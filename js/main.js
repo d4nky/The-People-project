6d28623ec871a4668d84f57160fe8976b4b4150c
d4nky <vertix08@gmail.com>
2021-10-24 17:57:51 +0100

    Added menu functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-onclick(MouseEvent):document.getElementsByClassName('menu-button');
+let button = document.querySelector(".menu-button");
+let menu = document.querySelector(".dropdown-menu");
+let menuStatus = false;
+
+function menuOpen(){
+    if (menuStatus == false){
+        menu.style.right = "-2500px";
+        menuStatus = true;
+    }
+    else if (menuStatus == true){
+        menu.style.right = "0px";
+        menuStatus = false;
+    }
+}
+
+button.onclick = menuOpen;
